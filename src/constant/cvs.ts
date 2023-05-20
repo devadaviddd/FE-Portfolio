4c197cc1b130e9b18e44dd4cb3b85dc115301e2c
devadaviddd <s3927112@rmit.edu.vn>
2023-05-20 22:41:03 +0700

    cv layout UI

---
+++ src/constant/cvs.ts
@@ -0,0 +1,21 @@
+type CV = {
+  imagePath: string;
+};
+
+export const cvs: CV[] = [
+  {
+    imagePath: "assets/cv1",
+  },
+  {
+    imagePath: "assets/cv2",
+  },
+  {
+    imagePath: "assets/cv3",
+  },
+  {
+    imagePath: "assets/cv4",
+  },
+  {
+    imagePath: "assets/cv5",
+  },
+];
--- src/layout/Signin/cv-layout.tsx
+++ src/layout/Signin/cv-layout.tsx
@@ -1,12 +1,24 @@
-import { Container } from "@mui/material"
+import { Box, Container } from "@mui/material";
+import { cvs } from "../../constant";
+import { useState } from "react";
+import { CVCarousel } from "../../components/CV-carousel";
+
 
 export const CVLayout = () => {
   return (
-    <Container maxWidth={false} sx={{
-      width: "100%",
-      height: "40vh",
-      background: "#dde9f5"
-    }}>
+    <Container
+      maxWidth={false}
+      sx={{
+        width: "100%",
+        height: "65vh",
+        background: "#dde9f5",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CVCarousel/>
     </Container>
-  )
-}
+  );
+};
